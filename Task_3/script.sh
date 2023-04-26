7fea3af90cfe0a1e058bf0486c8342551c1f68b9
Annazio <124679541+Annazio@users.noreply.github.com>
2023-04-26 13:47:12 +0200

    Update script.sh

--- Task_3/script.sh
+++ Task_3/script.sh
@@ -5,5 +5,5 @@ read name
 echo "“Welcome to terminal $name"
 mkdir -p /tmp/test
 touch /tmp/test/mydate.txt
-date + "%T" >> /tmp/test/mydate.txt
+date +"%T" >> /tmp/test/mydate.txt
 echo "Сохраняем данные"
