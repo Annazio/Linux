a59ceba4950bf1cb7bd1ec1df49f8fe492057660
Annazio <124679541+Annazio@users.noreply.github.com>
2023-04-26 11:03:48 +0200

    Update script.sh

--- Task_3/script.sh
+++ Task_3/script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Hello Student!"
-echo -n "Enter your name"
+echo -n "Enter your name "
 read name
 echo "“Welcome to terminal $name"
 mkdir -p /tmp/test
 touch /tmp/test/mydate.txt
-date + %T >> /tmp/test/mydate.txt
+date + "%T" >> /tmp/test/mydate.txt
 echo "Сохраняем данные"
